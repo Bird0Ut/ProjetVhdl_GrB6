3857d0d4b94636073000e18ed201ddde8b78780d
Bird0Ut <123623881+Bird0Ut@users.noreply.github.com>
2023-11-28 08:25:54 +0100

    new nios project

---
+++ niosxx2_inst.vhd
@@ -0,0 +1,17 @@
+	component niosxx2 is
+		port (
+			clk_clk                                : in  std_logic                    := 'X';             -- clk
+			leds_external_connection_export        : out std_logic_vector(7 downto 0);                    -- export
+			data_compas_external_connection_export : in  std_logic_vector(8 downto 0) := (others => 'X'); -- export
+			data_valid_external_connection_export  : in  std_logic                    := 'X'              -- export
+		);
+	end component niosxx2;
+
+	u0 : component niosxx2
+		port map (
+			clk_clk                                => CONNECTED_TO_clk_clk,                                --                             clk.clk
+			leds_external_connection_export        => CONNECTED_TO_leds_external_connection_export,        --        leds_external_connection.export
+			data_compas_external_connection_export => CONNECTED_TO_data_compas_external_connection_export, -- data_compas_external_connection.export
+			data_valid_external_connection_export  => CONNECTED_TO_data_valid_external_connection_export   --  data_valid_external_connection.export
+		);
+
